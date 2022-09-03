5e0fa022244ef391a8cb0326f7b0e0326847df47
Juha Kukkonen <juha7kukkonen@gmail.com>
2022-09-04 01:01:20 +0300

    Graceful shutdown when all commands exit & rcfile config load

---
+++ src/config.rs
@@ -0,0 +1,179 @@
+use std::{
+    env, io,
+    path::{Path, PathBuf},
+    process::{ExitStatus, Stdio},
+};
+
+use ansi_term::{Color, Style};
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+use tokio::{
+    fs,
+    io::{AsyncBufReadExt, BufReader},
+    process::{Child, Command},
+    sync::mpsc::{error::SendError, Sender},
+    task::JoinError,
+};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Config {
+    pub filter: Option<String>,
+    pub processes: Vec<WatchProcess>,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
+pub enum RunType {
+    #[serde(rename = "shell")]
+    Shell,
+    #[serde(rename = "cmd")]
+    Cmd,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct WatchProcess {
+    title: String,
+    cmd: String,
+    #[serde(default = "default_true")]
+    log: bool,
+    #[serde(rename = "type")]
+    run_type: Option<RunType>,
+}
+
+fn default_true() -> bool {
+    true
+}
+
+impl WatchProcess {
+    pub async fn run(&self, sender: Sender<String>) -> Result<(), WatchError> {
+        let ty = self.run_type.as_ref().unwrap_or(&RunType::Cmd);
+        if *ty == RunType::Cmd {
+            let (cmd, args) =
+                self.cmd
+                    .split(' ')
+                    .fold(("", Vec::<&str>::new()), |(mut cmd, mut args), item| {
+                        if cmd.is_empty() {
+                            cmd = item;
+                        } else {
+                            args.push(item)
+                        }
+
+                        (cmd, args)
+                    });
+
+            let child = Command::new(cmd)
+                .stdout(Stdio::piped())
+                .args(args.iter())
+                .spawn()
+                .map_err(WatchError::IoChildProcess)?;
+
+            self.execute_and_await(child, sender, &*self.title).await?
+        } else {
+            let child = Command::new("bash")
+                .stdout(Stdio::piped())
+                .arg("-c")
+                .arg(&self.cmd)
+                .spawn()
+                .map_err(WatchError::IoChildProcess)?;
+
+            self.execute_and_await(child, sender, &*self.title).await?
+        };
+
+        Ok(())
+    }
+
+    async fn execute_and_await(
+        &self,
+        mut child: Child,
+        sender: Sender<String>,
+        title: &str,
+    ) -> Result<ExitStatus, WatchError> {
+        let stdout = child.stdout.take().unwrap();
+        let mut lines = BufReader::new(stdout).lines();
+
+        let child_process = tokio::spawn(async move { child.wait().await });
+
+        while let Some(line) = &lines.next_line().await? {
+            let title = Style::new()
+                .on(Color::Fixed(173))
+                .paint(format!("[ {title} ]>"));
+
+            sender.send(format!("{title} {line}\n")).await?;
+        }
+
+        child_process.await?.map_err(WatchError::IoChildProcess)
+    }
+}
+
+#[derive(Error, Debug)]
+pub enum WatchError {
+    #[error("child process io error: {0:?}")]
+    IoChildProcess(#[from] io::Error),
+
+    #[error("{0:?}")]
+    ChildProcessExecute(#[from] JoinError),
+
+    #[error("send failed to parent")]
+    SendError(#[from] SendError<String>),
+}
+
+#[derive(Error, Debug)]
+pub enum ConfigError {
+    #[error("serde yaml")]
+    Parse(#[from] serde_yaml::Error),
+
+    #[error("config file not provided stdin")]
+    Missing,
+
+    #[error("no .watchmuxrc.yaml file in current directory")]
+    NoRcFile,
+
+    #[error("io failed to read file from path")]
+    Io(#[from] io::Error),
+}
+
+pub async fn read_config(path: Option<PathBuf>) -> Result<Config, ConfigError> {
+    match path {
+        Some(path) => {
+            if path.as_path().as_os_str() == "-" {
+                read_config_file_stdin().await
+            } else {
+                read_config_file_path(path.as_path()).await
+            }
+        }
+        None => read_config_from_rc_file().await,
+    }
+}
+
+async fn read_config_file_stdin() -> Result<Config, ConfigError> {
+    let stdin = tokio::io::stdin();
+    let reader = BufReader::new(stdin);
+    let mut lines = reader.lines();
+    let mut config = String::new();
+
+    while let Ok(Some(line)) = lines.next_line().await {
+        config.push_str(line.as_str());
+        config.push('\n');
+    }
+
+    if config.is_empty() {
+        Err(ConfigError::Missing)
+    } else {
+        serde_yaml::from_str(config.as_str()).map_err(ConfigError::Parse)
+    }
+}
+
+async fn read_config_file_path<P: AsRef<Path>>(path: P) -> Result<Config, ConfigError> {
+    let config = fs::read_to_string(path.as_ref()).await?;
+
+    serde_yaml::from_str(config.as_str()).map_err(ConfigError::Parse)
+}
+
+async fn read_config_from_rc_file() -> Result<Config, ConfigError> {
+    let mut current_dir = env::current_dir()?;
+    current_dir.push(".watchmuxrc.yaml");
+
+    match current_dir.try_exists() {
+        Ok(_) => read_config_file_path(current_dir.as_path()).await,
+        Err(_) => Err(ConfigError::NoRcFile),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,228 +1,63 @@
-use std::{
-    path::{Path, PathBuf},
-    process::Stdio,
-};
+use std::path::PathBuf;
 
-use ansi_term::{Color, Style};
 use clap::Parser;
-use serde::{Deserialize, Serialize};
+use config::{Config, ConfigError, WatchError};
+use futures::future;
 use thiserror::Error;
-use tokio::{
-    fs,
-    io::{self, stdin, stdout, AsyncBufReadExt, AsyncWriteExt, BufReader},
-    process::{Child, Command},
-    sync::mpsc::{self, error::SendError, Receiver, Sender},
-};
+use tokio::{io::AsyncWriteExt, sync::mpsc};
 
+mod config;
 #[derive(Parser, Debug)]
 struct WatchMux {
     /// Path to the config file of watchmux.
     #[clap(short, long, value_name = "FILE")]
-    config: PathBuf,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Config {
-    filter: Option<String>,
-    processes: Vec<WatchProcess>,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct WatchProcess {
-    title: String,
-    cmd: String,
-    #[serde(default = "default_true")]
-    log: bool,
-    #[serde(rename = "type")]
-    run_type: Option<RunType>,
-}
-
-impl WatchProcess {
-    async fn run(&self, sender: Sender<String>) -> Result<(), WatchError> {
-        let ty = self.run_type.as_ref().unwrap_or(&RunType::Cmd);
-        if *ty == RunType::Cmd {
-            let (cmd, args) =
-                self.cmd
-                    .split(' ')
-                    .fold(("", Vec::<&str>::new()), |(mut cmd, mut args), item| {
-                        if cmd.is_empty() {
-                            cmd = item;
-                        } else {
-                            args.push(item)
-                        }
-
-                        (cmd, args)
-                    });
-
-            let child = Command::new(cmd)
-                .stdout(Stdio::piped())
-                .args(args.iter())
-                .spawn()
-                .map_err(WatchError::IoChildProcess)?;
-
-            self.execute_and_await(child, sender, &*self.title).await?
-        } else {
-            let child = Command::new("bash")
-                .stdout(Stdio::piped())
-                .arg("-c")
-                .arg(&self.cmd)
-                .spawn()
-                .map_err(WatchError::IoChildProcess)?;
-
-            self.execute_and_await(child, sender, &*self.title).await?
-        };
-
-        Ok(())
-    }
-
-    async fn execute_and_await(
-        &self,
-        mut child: Child,
-        sender: Sender<String>,
-        title: &str,
-    ) -> Result<(), WatchError> {
-        let stdout = child.stdout.take().unwrap();
-        let mut lines = BufReader::new(stdout).lines();
-
-        let process_title = String::from(title);
-        tokio::spawn(async move {
-            if let Err(error) = child.wait().await {
-                eprintln!("child '{process_title}' process encountered error, {error}")
-            }
-        });
-
-        while let Some(line) = &lines.next_line().await? {
-            let title = Style::new()
-                .on(Color::Fixed(173))
-                .paint(format!("[ {title} ]>"));
-
-            sender.send(format!("{title} {line}\n")).await?;
-        }
-        Ok(())
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
-enum RunType {
-    #[serde(rename = "shell")]
-    Shell,
-    #[serde(rename = "cmd")]
-    Cmd,
-}
-
-fn default_true() -> bool {
-    true
+    config: Option<PathBuf>,
 }
 
 #[derive(Error, Debug)]
-enum ConfigError {
-    #[error("serde yaml")]
-    Parse(#[from] serde_yaml::Error),
-
-    #[error("config file not provided stdin")]
-    Missing,
-
-    #[error("io failed to read file from path")]
-    Io(#[from] io::Error),
-}
-
-#[derive(Error, Debug)]
-enum WatchError {
-    #[error("config")]
+enum WatchmuxError {
+    #[error("failed to resolve config: {0:?}")]
     Config(#[from] ConfigError),
-
-    #[error("io failed to spawn child process")]
-    IoChildProcess(#[from] io::Error),
-
-    #[error("send failed to parent")]
-    SendError(#[from] SendError<String>),
+    #[error("failed to run watch process: {0:?}")]
+    WatchError(#[from] WatchError),
 }
 
 #[tokio::main]
-async fn main() -> Result<(), WatchError> {
+async fn main() -> Result<(), WatchmuxError> {
     let cli = WatchMux::parse();
 
-    let config = if cli.config.as_path().as_os_str() == "-" {
-        read_config_file_stdin().await?
-    } else {
-        read_config_file_path(cli.config.as_path()).await?
-    };
+    let config = config::read_config(cli.config).await?;
 
-    run(config).await
-}
-
-async fn read_config_file_stdin() -> Result<Config, ConfigError> {
-    let stdin = stdin();
-    let reader = BufReader::new(stdin);
-    let mut lines = reader.lines();
-    let mut config = String::new();
-
-    while let Ok(Some(line)) = lines.next_line().await {
-        config.push_str(line.as_str());
-        config.push('\n');
-    }
-
-    if config.is_empty() {
-        Err(ConfigError::Missing)
-    } else {
-        serde_yaml::from_str(config.as_str()).map_err(ConfigError::Parse)
-    }
-}
-
-async fn read_config_file_path<P: AsRef<Path>>(path: P) -> Result<Config, ConfigError> {
-    let config = fs::read_to_string(path.as_ref()).await?;
-
-    serde_yaml::from_str(config.as_str()).map_err(ConfigError::Parse)
+    run(config).await.map_err(WatchmuxError::WatchError)
 }
 
 async fn run(config: Config) -> Result<(), WatchError> {
-    // let (tx, rx) = mpsc::channel::<String>(num_cpus::get_physical() * 2);
     let (tx, mut rx) = mpsc::channel::<String>(1024);
 
-    for process in config.processes.into_iter() {
-        let sender = tx.clone();
-        tokio::spawn(async move {
-            process
-                .run(sender)
-                .await
-                .unwrap_or_else(|e| panic!("process {process:?} errored, error: {e:?}"));
-        });
+    let processes = future::join_all(
+        config
+            .processes
+            .into_iter()
+            .map(|process| {
+                let sender = tx.clone();
+                tokio::spawn(async move { process.run(sender).await })
+            })
+            .collect::<Vec<_>>(),
+    );
+    tokio::pin!(processes);
+
+    let mut stdout = tokio::io::stdout();
+    loop {
+        tokio::select! {
+            _ = &mut processes => {
+                rx.close();
+                break;
+            },
+            Some(line) = rx.recv() => {
+                stdout.write_all(line.as_bytes()).await?
+            }
+        };
     }
 
-    // let mut w = StdoutWriter(rx);
-    // let writer = w.write();
-    // tokio::pin!(writer);
-
-    // tokio::select! {
-    //     _ = &mut writer => {
-    //         // TODO
-    //     }
-    // }
-    // TODO
-    // let cpus = num_cpus::get_physical();
-    // for _ in 0..cpus {
-    //     tokio::spawn(async move {
-
-    //         // TODO
-    //     });
-    // }
-
-    let mut out = stdout();
-    while let Some(line) = rx.recv().await {
-        out.write_all(line.as_bytes()).await?
-    }
     Ok(())
-}
-
-struct StdoutWriter(Receiver<String>);
-
-impl StdoutWriter {
-    async fn write(&mut self) {
-        let mut out = io::stdout();
-
-        while let Some(message) = self.0.recv().await {
-            // TOOD write to the stdout
-            let a = out.write_all(message.as_bytes()).await;
-        }
-    }
 }
